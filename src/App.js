806ef229c96c8394f389722e4d3e55a6c0e5cf1d
bbayong28 <ghkdud5795@hanmail.net>
2022-09-07 15:29:40 +0900

    20220907

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+import './index.css';
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function App() {
+  return (
+     <div className="Wrapper">
+      <Header />
+      <Main/>
+      <Footer />
+    </div>   
+  );
+}
+
+export default App;
+
+
